8b01079fa6159bf28e006ad198e19202a9b9b366
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-10-04 12:53:17 +0200

    add-sass

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+// import { ReactComponent as MySVG } from './../../../public/img/logowallet.svg';
+
+const Header = () => {
+    return(
+        <div>
+            <div>
+                <img 
+                // width={30} height={30}
+                src="./../../../public/img/logowallet.svg" alt="logo"/>
+                <h1>Wallet</h1>
+                {/* <MySVG /> */}
+                
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default Header
