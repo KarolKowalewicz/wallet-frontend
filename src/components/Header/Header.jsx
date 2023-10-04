2789685ee8895af672f8e724e45fe36c257bdecf
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-10-04 13:01:30 +0200

    change-img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 
 // import { ReactComponent as MySVG } from './../../../public/img/logowallet.svg';
 
+import logo from './../../img/logowallet.png'
+
+
 const Header = () => {
     return(
         <div>
             <div>
                 <img 
                 // width={30} height={30}
-                src="./../../../public/img/logowallet.svg" alt="logo"/>
+                src={logo} alt="logo"/>
                 <h1>Wallet</h1>
-                {/* <MySVG /> */}
                 
             </div>
             <div>
