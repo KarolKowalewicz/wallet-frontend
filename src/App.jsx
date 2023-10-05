6d958700e30191d1fea722469f8450693c811d75
MiMad89 <michal9977@gmail.com>
2023-10-05 22:57:13 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
-import { lazy } from "react";
-import { Route, Routes, Navigate } from "react-router";
-import SharedLayout from "./components/SharedLayout/SharedLayout";
 
-// const Home = lazy(() => import("./path/to/home/component"));
-// const Diagram = lazy(() => import("./path/to/diagram/component"));
-// const Login = lazy(() => import("./path/to/login/component"));
-// const Register = lazy(() => import("./path/to/home/component"));
+// import { lazy } from "react";
+// import { Route, Routes, Navigate } from "react-router";
+// import SharedLayout from "./components/SharedLayout/SharedLayout";
 
-function App() {
-  const isUserLoggedIn = true;
-  return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route
-          index
-          element={
-            isUserLoggedIn ? (
-              <p>Home page component...</p>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="diagram"
-          element={
-            isUserLoggedIn ? (
-              <p>Diagram page component...</p>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-      </Route>
-      <Route path="/login" element={<p>Login component...</p>} />
-      <Route path="/register" element={<p>Register component...</p>} />
-    </Routes>
-  );
-}
+// // const Home = lazy(() => import("./path/to/home/component"));
+// // const Diagram = lazy(() => import("./path/to/diagram/component"));
+// const Login = lazy(() => import("./pages/Login"));
+// // const Register = lazy(() => import("./path/to/home/component"));
 
-export default App;
+// function App() {
+//   const isUserLoggedIn = false;
+//   return (
+//     <Routes>
+//       <Route path="/" element={<SharedLayout />}>
+//         <Route
+//           index
+//           element={
+//             isUserLoggedIn ? (
+//               <p>Home page component...</p>
+//             ) : (
+//               <Navigate to="/login" />
+//             )
+//           }
+//         />
+//         <Route
+//           path="diagram"
+//           element={
+//             isUserLoggedIn ? (
+//               <p>Diagram page component...</p>
+//             ) : (
+//               <Navigate to="/login" />
+//             )
+//           }
+//         />
+//       </Route>
+//       <Route path="/login" element={
+//             isUserLoggedIn ? (
+//               <p>Login page component...</p>
+//             ) : (
+//               <Navigate to="/login" />
+//             )} />
+//       <Route path="/register" element={<p>Register component...</p>} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Fragment } from "react";
+import  LoginForm  from "../components/LoginForm/LoginForm";
+
+const Login = () => {
+  return (
+    <Fragment>
+      <LoginForm />
+    </Fragment>
+  );
+};
+
+export default Login;
