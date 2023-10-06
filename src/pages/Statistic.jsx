7e52cc06cba32bf2894ae3c8215ff6c2cb048bd4
MiMad89 <michal9977@gmail.com>
2023-10-07 00:38:59 +0200

    Create page statistic

---
+++ src/pages/Statistic.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Fragment } from "react";
+import  StatisticForm from "../components/StatisticForm/StatisticForm";
+
+const Statistic = () => {
+  return (
+    <Fragment>
+      <StatisticForm />
+    </Fragment>
+  );
+};
+
+export default Statistic;
