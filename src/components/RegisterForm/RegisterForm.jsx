6dbaf50d206f1823457884b7a3fa106b3c9d6a77
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-10-07 19:30:38 +0200

    ALMOST finished graphic layer

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,11 @@
 import css from './RegisterForm.module.scss';
 import logo from '../../img/logowallet.png';
 
+import { ReactComponent as Avatar } from '../../img/avatar.svg';
+import { ReactComponent as Lock } from '../../img/lock.svg'
+import { ReactComponent as Email } from '../../img/email.svg'
+
+
 const RegisterForm = () => {
     return (
         <div className={css.formBox}>
@@ -9,16 +14,20 @@ const RegisterForm = () => {
                 <span className={css.logoTitle}>Wallet</span>
             </div>
             <form className={css.form}>
-                <label className={css.label}>                    
+                <label className={css.label}>
+                    <Email className={css.svg}/>                    
                     <input className={css.input} placeholder='E-mail' type='email' name='email'/>
                 </label>
                 <label className={css.label}>
+                    <Lock className={css.svg}/>
                     <input className={css.input} placeholder='Password' type='password' name='password' />
                 </label>
                 <label className={css.label}>
-                    <input className={css.input} placeholder='Confirm Password'/>
+                    <Lock className={css.svg}/>
+                    <input className={css.input} placeholder='Confirm Password' type='password' name='confirm-password'/>
                 </label>
                 <label className={css.label}>
+                    <Avatar className={css.svg}/>                    
                     <input className={css.input} placeholder='First name' type='text' name='name'/>
                 </label>
                 <div className={css.buttonSection}>
