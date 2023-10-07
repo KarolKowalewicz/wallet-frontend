0aaf88dcd9be9d27ce4b9e0436149e612c453f2c
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-10-07 13:53:45 +0200

    feat: created redux store and implemented redux slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/api/apiSlice";
+import authSliceReducer from "./slices/auth/authSlice";
 
-const store = configureStore({ reducer: {} });
+const store = configureStore({
+  reducer: {
+    auth: authSliceReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
 
 export default store;
