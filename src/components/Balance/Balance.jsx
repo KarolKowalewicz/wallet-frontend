f0eb0a0a8a2ae995a00a150f97fcdc23614afab1
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-10-11 01:07:11 +0200

    feat: display balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,16 +1,17 @@
+import styles from "./Balance.module.scss";
 
-import styles from './Balance.module.scss'
+const Balance = ({ balance, isLoading }) => {
+  return (
+    <div className={styles.balance}>
+      <div className={styles.balance__layout}>
+        <p className={styles.balance__description}>your balance</p>
+        <h3 className={styles.balance__content}>
+          {/* TODO: Add some spinner */}
+          &euro; {isLoading ? "Loading..." : balance}
+        </h3>
+      </div>
+    </div>
+  );
+};
 
-const Balance = () => {
-
-    return(
-        <div className={styles.balance}>
-            <div className={styles.balance__layout}>
-                <p className={styles.balance__description}>your balance</p>
-                <h3 className={styles.balance__content}>&euro; 24 000.00</h3>
-            </div>
-        </div>
-    )
-}
-
-export default Balance
+export default Balance;
