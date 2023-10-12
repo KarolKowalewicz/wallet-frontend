5c5fb81f28ca64b57df9202daa3bbde451d7162e
michalmazurdev <m.mazur40@gmail.com>
2023-10-12 20:02:24 +0200

    styles added

--- src/components/LogoutModal/LogoutModal.jsx
+++ src/components/LogoutModal/LogoutModal.jsx
@@ -3,9 +3,20 @@ const LogoutModal = ({ logout, onClose }) => {
   return (
     <div className={styles.logoutModalContainer}>
       <div className={styles.logoutModal}>
-        <p>Are you sure you want to leave?</p>
-        <button onClick={logout}>yes, take out of here</button>
-        <button onClick={onClose}>I wanna stay here for a little more</button>
+        <p className={styles.header}>Are you sure you want to log out??</p>
+        <button className={styles.button} onClick={logout}>
+          <p className={styles.button__label}>Please log me out</p>
+        </button>
+        <button
+          className={`${styles.button} ${styles.button__close}`}
+          onClick={onClose}
+        >
+          <p
+            className={`${styles.button__label} ${styles.button__label__close} `}
+          >
+            I want to stay here
+          </p>
+        </button>
       </div>
     </div>
   );
