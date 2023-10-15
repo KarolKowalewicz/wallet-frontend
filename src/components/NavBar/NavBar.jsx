01cfe236300a64f760261342ee51eba21beadf40
KarolKowalewicz <kowalewiczkarol@gmail.com>
2023-10-15 12:43:50 +0200

    shadow icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import styles from "./NavBar.module.scss";
+import { ReactComponent as IconHome } from "../../img/icon-home.svg";
+import { ReactComponent as IconStatistics } from "../../img/icon-statistics.svg";
+import { ReactComponent as IconExchange } from "../../img/icon-exchange.svg";
 
 function NavBar() {
   const [activeIcon, setActiveIcon] = useState("home");
@@ -22,49 +25,40 @@ function NavBar() {
       <div>
         <ul className={styles["navbar-container"]}>
           <li>
-            <NavLink exact="true" to="/">
-              <img
-                src={
+            <NavLink exact to="/" activeClassName={styles.active}>
+              <IconHome
+                fillColor={
                   activeIcon === "home"
-                    ? require("./../../img/home_dark.png")
-                    : require("./../../img/home_light.png")
+                    ? "$nav-icon-color-active"
+                    : "$nav-icon-color-inactive"
                 }
-                className={`${styles.navbaricon} ${
-                  activeIcon === "home" ? styles.activeIcon : ""
-                }`}
-                alt="Home"
+                className={styles.nav__svg}
               />
               <span className={styles.text}>Home</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/statistic">
-              <img
-                src={
+            <NavLink to="/statistic" activeClassName={styles.active}>
+              <IconStatistics
+                fillColor={
                   activeIcon === "statistics"
-                    ? require("./../../img/statistics_dark.png")
-                    : require("./../../img/statistics_light.png")
+                    ? "$nav-icon-color-active"
+                    : "$nav-icon-color-inactive"
                 }
-                className={`${styles.navbaricon} ${
-                  activeIcon === "statistics" ? styles.activeIcon : ""
-                }`}
-                alt="Statistics"
+                className={styles.nav__svg}
               />
               <span className={styles.text}>Statistics</span>
             </NavLink>
           </li>
           <li className={styles.mobileOnly}>
-            <NavLink to="/exchange">
-              <img
-                src={
+            <NavLink to="/exchange" activeClassName={styles.active}>
+              <IconExchange
+                fillColor={
                   activeIcon === "exchange"
-                    ? require("./../../img/dollar_dark.png")
-                    : require("./../../img/dollar_light.png")
+                    ? "$nav-icon-color-active"
+                    : "$nav-icon-color-inactive"
                 }
-                className={`${styles.navbaricon} ${
-                  activeIcon === "exchange" ? styles.activeIcon : ""
-                }`}
-                alt="Current Exchange"
+                className={styles.nav__svg}
               />
             </NavLink>
           </li>
@@ -75,3 +69,66 @@ function NavBar() {
 }
 
 export default NavBar;
+
+
+// kolorki na sztywno
+// import React, { useState, useEffect } from "react";
+// import { NavLink, useLocation } from "react-router-dom";
+// import styles from "./NavBar.module.scss";
+// import { ReactComponent as IconHome } from "../../img/icon-home.svg";
+// import { ReactComponent as IconStatistics } from "../../img/icon-statistics.svg";
+// import { ReactComponent as IconExchange } from "../../img/icon-exchange.svg";
+
+// function NavBar() {
+//   const [activeIcon, setActiveIcon] = useState("home");
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     const path = location.pathname;
+//     if (path === "/") {
+//       setActiveIcon("home");
+//     } else if (path === "/statistic") {
+//       setActiveIcon("statistics");
+//     } else if (path === "/exchange") {
+//       setActiveIcon("exchange");
+//     }
+//   }, [location]);
+
+//   return (
+//     <nav>
+//       <div>
+//         <ul className={styles["navbar-container"]}>
+//           <li>
+//             <NavLink exact to="/" activeClassName={styles.active}>
+//               <IconHome
+//                 fill={activeIcon === "home" ? "#4A56E2" : "#6E78E8"}
+//                 className={styles.nav__svg}
+//               />
+
+//               <span className={styles.text}>Home</span>
+//             </NavLink>
+//           </li>
+//           <li>
+//             <NavLink to="/statistic" activeClassName={styles.active}>
+//               <IconStatistics
+//                 fill={activeIcon === "statistics" ? "#4A56E2" : "#6E78E8"}
+//                 className={styles.nav__svg}
+//               />
+//               <span className={styles.text}>Statistics</span>
+//             </NavLink>
+//           </li>
+//           <li className={styles.mobileOnly}>
+//             <NavLink to="/exchange" activeClassName={styles.active}>
+//               <IconExchange
+//                 fill={activeIcon === "exchange" ? "#4A56E2" : "#6E78E8"}
+//                 className={styles.nav__svg}
+//               />
+//             </NavLink>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
