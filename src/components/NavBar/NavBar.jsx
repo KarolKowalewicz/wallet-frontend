88c568d712219b34809d9a67d109ef479bbfd2e7
KarolKowalewicz <kowalewiczkarol@gmail.com>
2023-10-15 14:49:50 +0200

    shadow icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -24,44 +24,52 @@ function NavBar() {
     <nav>
       <div>
         <ul className={styles["navbar-container"]}>
+          
           <li>
             <NavLink exact to="/" activeClassName={styles.active}>
               <IconHome
-                fillColor={
-                  activeIcon === "home"
-                    ? "$nav-icon-color-active"
-                    : "$nav-icon-color-inactive"
-                }
-                className={styles.nav__svg}
+                fill={activeIcon === "home" ? "#4A56E2" : "#6E78E8"}
+                className={`${styles.nav__svg} ${
+                  activeIcon === "home" ? styles.active : ""
+                }`}
               />
-              <span className={styles.text}>Home</span>
+              <span
+                className={`${styles.text} ${
+                  activeIcon === "home" ? styles.active : ""
+                }`}
+              >
+                Home
+              </span>
             </NavLink>
           </li>
           <li>
             <NavLink to="/statistic" activeClassName={styles.active}>
               <IconStatistics
-                fillColor={
-                  activeIcon === "statistics"
-                    ? "$nav-icon-color-active"
-                    : "$nav-icon-color-inactive"
-                }
-                className={styles.nav__svg}
+                fill={activeIcon === "statistics" ? "#4A56E2" : "#6E78E8"}
+                className={`${styles.nav__svg} ${
+                  activeIcon === "statistics" ? styles.active : ""
+                }`}
               />
-              <span className={styles.text}>Statistics</span>
+              <span
+                className={`${styles.text} ${
+                  activeIcon === "statistics" ? styles.active : ""
+                }`}
+              >
+                Statistics
+              </span>
             </NavLink>
           </li>
           <li className={styles.mobileOnly}>
             <NavLink to="/exchange" activeClassName={styles.active}>
               <IconExchange
-                fillColor={
-                  activeIcon === "exchange"
-                    ? "$nav-icon-color-active"
-                    : "$nav-icon-color-inactive"
-                }
-                className={styles.nav__svg}
+                fill={activeIcon === "exchange" ? "#4A56E2" : "#6E78E8"}
+                className={`${styles.nav__svg} ${
+                  activeIcon === "exchange" ? styles.active : ""
+                }`}
               />
             </NavLink>
           </li>
+          
         </ul>
       </div>
     </nav>
@@ -71,64 +79,3 @@ function NavBar() {
 export default NavBar;
 
 
-// kolorki na sztywno
-// import React, { useState, useEffect } from "react";
-// import { NavLink, useLocation } from "react-router-dom";
-// import styles from "./NavBar.module.scss";
-// import { ReactComponent as IconHome } from "../../img/icon-home.svg";
-// import { ReactComponent as IconStatistics } from "../../img/icon-statistics.svg";
-// import { ReactComponent as IconExchange } from "../../img/icon-exchange.svg";
-
-// function NavBar() {
-//   const [activeIcon, setActiveIcon] = useState("home");
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     const path = location.pathname;
-//     if (path === "/") {
-//       setActiveIcon("home");
-//     } else if (path === "/statistic") {
-//       setActiveIcon("statistics");
-//     } else if (path === "/exchange") {
-//       setActiveIcon("exchange");
-//     }
-//   }, [location]);
-
-//   return (
-//     <nav>
-//       <div>
-//         <ul className={styles["navbar-container"]}>
-//           <li>
-//             <NavLink exact to="/" activeClassName={styles.active}>
-//               <IconHome
-//                 fill={activeIcon === "home" ? "#4A56E2" : "#6E78E8"}
-//                 className={styles.nav__svg}
-//               />
-
-//               <span className={styles.text}>Home</span>
-//             </NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="/statistic" activeClassName={styles.active}>
-//               <IconStatistics
-//                 fill={activeIcon === "statistics" ? "#4A56E2" : "#6E78E8"}
-//                 className={styles.nav__svg}
-//               />
-//               <span className={styles.text}>Statistics</span>
-//             </NavLink>
-//           </li>
-//           <li className={styles.mobileOnly}>
-//             <NavLink to="/exchange" activeClassName={styles.active}>
-//               <IconExchange
-//                 fill={activeIcon === "exchange" ? "#4A56E2" : "#6E78E8"}
-//                 className={styles.nav__svg}
-//               />
-//             </NavLink>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default NavBar;
